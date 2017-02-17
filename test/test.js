950f54e92539541afc8643a94160059bf5cea77d
Hunter Manhart <Hunter.M.Manhart@vanderbilt.edu>
2017-02-16 21:09:38 -0600

    Trying to run emulator

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 import 'react-native';
 import React from 'react';
-import Intro from '../Intro';
+import App from '../App/helloworld';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
